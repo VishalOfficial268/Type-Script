1b3bc9294e5d8678fbaf6d1c48c95ee15b3ffffe
AadiVishu4 <vishalofiicial268@gmail.com>
2022-07-31 17:34:17 +0530

    symbol used

---
+++ symbol.js
@@ -0,0 +1,5 @@
+var sym1 = Symbol();
+var sym2 = Symbol("d1");
+var sym3 = Symbol("d1");
+console.log(sym2 === sym3);
+console.log(sym1, sym2, sym3);
---
+++ symbol.ts
@@ -0,0 +1,5 @@
+let sym1 = Symbol();
+let sym2 = Symbol("d1");
+let sym3 = Symbol("d1");
+console.log(sym2 === sym3)
+console.log(sym1, sym2, sym3)
